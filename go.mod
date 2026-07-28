f53136a64e6ccc3956041c4754ec514086eb08ee
David Torcivia <david.torcivia@gmail.com>
2026-07-27 21:10:56 -0400

    chore: initialize go module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module davidtorcivia.com/dtcom
+
+go 1.22.4
---
+++ internal/main.go
@@ -0,0 +1,5 @@
+// Package main is the dtcom binary entrypoint.
+package main
+
+// Version is the build version, overridden via -ldflags.
+const Version = "dev"
